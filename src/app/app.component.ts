8076a0f67bb6604972f1e744c4c797750e81f7c4
wpwilcz <wpwilcz@gmail.com>
2018-01-15 18:46:35 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  @ViewChild('f') form: NgForm;
+  paths = [
+    111, 222, 333
+  ];
+
+  onSubmit() {
+    console.log(this.form);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { SampleComponent } from './sample/sample.component';
+
+const routes: Routes = [
+  { path: 'aaa/bbb/ccc', component: SampleComponent }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SampleComponent,
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      routes,
+      { enableTracing: true }
+    ),
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
