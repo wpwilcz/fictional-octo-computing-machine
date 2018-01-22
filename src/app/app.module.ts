edc7ab3c8fc26be959aef2f20429dece4b9d2b7a
wpwilcz <wpwilcz@gmail.com>
2018-01-22 22:41:12 +0100

    Add date picker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { SampleComponent } from './sample/sample.component';
+import { OwlDateTimeModule, OwlNativeDateTimeModule } from 'ng-pick-datetime';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const routes: Routes = [
   { path: 'aaa/bbb/ccc', component: SampleComponent }
@@ -24,6 +26,9 @@ const routes: Routes = [
     ),
     BrowserModule,
     FormsModule,
+    BrowserAnimationsModule,
+    OwlDateTimeModule,
+    OwlNativeDateTimeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
